5f33d10ac9f354738bde84624da6deee1f11289f
alvaro <alvar@Alvaro>
2022-07-31 08:33:38 -0300

    cria objeto tag

---
+++ src/main/java/com/mycompany/trabalhoaspectos/Tags.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhoaspectos;
+
+/**
+ *
+ * @author alvar
+ */
+public class Tags {
+    private String id;
+    private String def;
+    private Automato a;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDef() {
+        return def;
+    }
+
+    public void setDef(String def) {
+        this.def = def;
+    }
+
+    public Automato getA() {
+        return a;
+    }
+
+    public void setA(Automato a) {
+        this.a = a;
+    }
+    
+    
+}
