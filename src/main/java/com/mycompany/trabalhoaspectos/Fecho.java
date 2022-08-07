b19641d70770549e579506e2b720b157ad957cc7
alvaro <alvar@Alvaro>
2022-08-07 09:31:42 -0300

    Completa função toAFN e cria classe Fecho

---
+++ src/main/java/com/mycompany/trabalhoaspectos/Fecho.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalhoaspectos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alvar
+ */
+public class Fecho {
+    private ArrayList<Integer> lista;
+
+    public ArrayList<Integer> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Integer> lista) {
+        this.lista = lista;
+    }
+    
+    
+}
